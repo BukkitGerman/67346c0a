7f568509e23e611e5853a595b4119389453a33b7
BukkitGerman <tanks110011@gmail.com>
2020-06-03 15:18:00 +0200

    add showNavigation Function

--- php/main.php
+++ php/main.php
@@ -1,4 +1,26 @@
 <?php
 
+function showNavigation($aktive){
+
+
+	echo "<nav>
+			<ul>";
+
+			if($aktive == "home"){
+		
+			}elseif ($aktive == "") {
+				
+			}elseif ($aktive == "") {
+				
+			}elseif ($aktive == "") {
+				
+			}elseif ($aktive == "") {
+				
+			}
+
+	echo "	</ul>
+		  </nav>";
+}
+
 
 ?>

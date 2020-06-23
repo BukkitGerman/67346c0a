38edcc255380543a334649540faff5bfec4da6d3
BukkitGerman <tanks110011@gmail.com>
2020-06-23 05:15:54 +0200

    Changes to be committed: 	modified:   p2/page/impressum.php 	modified:   p2/page/verify.php 	new file:   p2/templates/impressum.html
    Change Impressum side, Impressum Text is now Loaded from impressum.html template
    
    Fix a Bug in Verify.php and the Email Verifycation!

--- p2/page/verify.php
+++ p2/page/verify.php
@@ -7,16 +7,24 @@ include 'ts3.php';
 
 if(isset($_GET['type']) && isset($_GET['usr']) && isset($_GET['token'])){
 	if($_GET['type'] == 'email'){
-		$smt = $db->prepare("SELECT email_token FROM users WHERE id = :uid");
+		$smt = $db->prepare("SELECT verify_email FROM users WHERE id = :uid");
 		$smt->bindValue(':uid', $_GET['usr']);
 		$smt = $smt->execute();
 		$smt = $smt->fetchArray();
+		if($smt['verify_email'] == 0){
+			$smt = $db->prepare("SELECT email_token FROM users WHERE id = :uid");
+			$smt->bindValue(':uid', $_GET['usr']);
+			$smt = $smt->execute();
+			$smt = $smt->fetchArray();
 
-		if(verifyEmail($db, $_GET['usr'], $_GET['token'])){
-			$content = "<p>Du hast dich erfolgreich Verifiziert!</p>";
-		}else{
-			$content = "<p>Es ist ein Fehler aufgetreten!</p>";
-		}
+			if(verifyEmail($db, $_GET['usr'], $_GET['token'])){
+				$content = "<p>Du hast dich erfolgreich Verifiziert!</p>";
+			}else{
+				$content = "<p>Es ist ein Fehler aufgetreten!</p>";
+			}
+	}elseif($smt['verify_email'] == 1){
+		$content = "<p>Bereits Verifiziert!</p>";
+	}
 	}elseif ($_GET['type'] == 'teamspeak') {
 
 	}

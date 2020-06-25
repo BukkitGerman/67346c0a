6e094403562544c0f674022985caa75db5907713
BukkitGerman <tanks110011@gmail.com>
2020-06-25 02:35:17 +0200

    Changes to be committed: 	modified:   p2/database/database.db 	new file:   p2/page/devlog.php
    Add Devlog.php page
    Add Devlog.php functionality

---
+++ p2/page/devlog.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+$db = new SQLite3("../database/database.db");
+
+include 'main.php';
+include 'ts3.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+<?php
+if(isset($_SESSION['uid']))
+{
+	echo getNavigationbar($db, $_SESSION['uid']);
+}else{
+ 	echo getNavigationbar($db);
+}
+?>
+<div class="content">
+	<?php
+		$posts = getDevlogPosts($db);
+		while ($dbsatz = $posts->fetchArray()) {
+			$text .=" <div class='inner-item'>
+						<div class='post'>
+						<h3>".$dbsatz['header']."</h3>
+						<div id='post_body'>
+						".$dbsatz['log']."
+						</div>
+						</div>
+					</div>";
+		}
+		echo $text;
+	?>
+</div>
+
+
+
+
+
+
+<?php
+	echo getFooter();
+?>
+</body>
+</html>
